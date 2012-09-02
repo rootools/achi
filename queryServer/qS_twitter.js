d93549219db9d9fb1c1919b2a1d61459d0fc35d5
rootools <baboonweb@gmail.com>
2012-09-02 22:49:05 +0000

    complite earned achievements mechanism

--- queryServer/qS_twitter.js
+++ queryServer/qS_twitter.js
@@ -19,10 +19,44 @@ function checkTwitterAchievements(uid, data, db, cb) {
          },
     }, function(err, res) {
       res.all = createAIDarray(res.all);
-      console.log(res);
+      res.users = createAIDarray(res.users);
+      if(res.users == undefined || res.users.length == 0) {res.users = [];}
+      var notRecieved = res.all;
+      for(var i=0;i<notRecieved.length;i++) {
+        for(var r=0;r<res.users.length;r++) {
+          if(res.users[r] == notRecieved[i]) {
+            notRecieved.splice(i,1);
+          }
+        }
+      }
+      for(var i=0;i<notRecieved.length;i++) {
+        var runTest = eval('twit_'+notRecieved[i]);
+        runTest(uid, data, notRecieved[i], db);
+      }
     });
 
   cb();
+}
+
+// Write 10 tweets
+function twit_0OeqcxuY778XB5fHDJPRlk2EwWzFLd(uid, data, aid, db) {
+  if(data.statuses_count > 10) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+function twit_XvEsAGN7V9nc5xmyl2Nltcd9kQHqnf(uid, data, aid, db) {
+// Write 20 tweets
+  if(data.statuses_count > 20) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+function writeToDB(uid, aid, db) {
+  db.collection('users_achievements', function(err,collection) {
+    collection.update({uid:uid, service: 'twitter'}, {$push: {achievements:{aid:aid, time:new Date().getTime()}} }, function(err, doc) {
+    });
+  });
 }
 
 
