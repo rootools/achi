b76d5cd08d124e849672737180f36e71a198caab
rootools <baboonweb@gmail.com>
2012-12-09 15:21:23 +0400

    fix informer bug

--- public/javascripts/dashboard_new.js
+++
@@ -1,9 +0,0 @@
-function sync(param, cb) {
-  $.post('/webapi', param, function(data) {
-    cb(data);
-  }, 'json');
-}
-
-$(function(){
-	
-});
--- public/javascripts/informer.js
+++ public/javascripts/informer.js
@@ -1,12 +1,6 @@
-function sync(path, data, cb) {
-  $.post(path, data, function(data, status, jqXHR) {
-    cb(JSON.parse(jqXHR.responseText));
-  });
-}
-
 $(function() {
-  sync('webapi', {action: 'get_new_notifications'}, function(response) {
-    console.log(response);
+  
+  sync('/webapi', {action: 'get_new_notifications'}, function(response) {
     var notifications = document.getElementById('notifications');
     notifications.innerHTML = '';
     if(response > 0) {
@@ -15,7 +9,12 @@ $(function() {
       html += '<a href="/profile#messages" id="profile_messages"><span class="fg-color-red"><i class="icon-mail"></i>'+response+'</span></a>';
       notifications.innerHTML = html;
       $('#profile_messages').bind('click', function() {
-        location.reload();
+        if(location.pathname === '/profile') {
+          location = '/profile#messages';
+          location.reload();
+        } else {
+          document.location.replace('/profile#messages');
+        }
       });
     }
   });
