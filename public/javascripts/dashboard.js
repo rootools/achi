d2202d9ab7fa1248d901fe7804849e1a6eaa57d3
rootools <baboonweb@gmail.com>
2012-12-12 16:35:05 +0400

    Fix #32

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -83,17 +83,44 @@ function last_achiv_event(elem) {
       $('.last_achiv_tile').removeClass('double');
       $('.last_achiv_tile').css('z-index', '');
       $('.last_achiv_tile').children('.tile-content').children('.last_achiv_tile_name').remove();
+      $('.last_achiv_tile').children('.brand').children('.social_sharing').html('');
       
       var achiv_name = $(elem).attr('title');
+      var social_sharing = create_social_sharing_pannerl(elem, achiv_name);
       $(elem).children('.tile-content').append('<h3 class="last_achiv_tile_name">'+achiv_name)+'</h3>';
+      $(elem).children('.brand').children('.social_sharing').html(social_sharing);
       $(elem).addClass('double');
-      $(elem).css('z-index', '999');
-      
+      $(elem).css('z-index', '888');
     } else {
       $(elem).children('.tile-content').children('.last_achiv_tile_name').remove();
+      $(elem).children('.brand').children('.social_sharing').html('');
       $(elem).removeClass('double');
       $(elem).css('z-index', '');
     }
+}
+
+function create_social_sharing_pannerl(elem, name) {
+  var icon = location.origin + $(elem).children('.tile-content').children('.achiv_icon').attr('src');
+  var points = $(elem).children('.brand').children('.badge').text();
+  var url = location.origin+'/dashboard/user/' + $('.grid').attr('id');
+  var title = 'Я заработал достижение!';
+  var text = '"'+name + '" за '+points+' очков! #ачивстер #achivster';
+  var html = '';
+  // VK
+  // http://vk.com/pages?oid=-1&p=%D0%9F%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D1%8F_%D1%81%D1%82%D0%BE%D1%80%D0%BE%D0%BD%D0%BD%D0%B8%D1%85_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86
+  var vk_sharing_object_1 = {url: url, title: 'Я заработал достижение!', image: icon, noparse: true, description: text};
+  var vk_sharing_object_2 = {type: 'custom', text: '<i class="websymbols social_sharing_button" id="vkontakte_sharing">v</i>'};
+  html += VK.Share.button(vk_sharing_object_1, vk_sharing_object_2);
+  // Facebook
+  // http://developers.facebook.com/docs/reference/dialogs/feed/
+  html += '<a href="https://www.facebook.com/dialog/feed?app_id=258024554279925&link=https://developers.facebook.com/docs/reference/dialogs/&picture='+icon+'&name='+title+'&caption=http://achivster.com/&description='+encodeURIComponent(text)+'&redirect_uri=http://rootools.ru/dashboard&link='+url+'">';
+  html += '<i class="websymbols social_sharing_button" id="facebook_sharing">f</i></a>';
+  // Twitter
+  // https://dev.twitter.com/docs/tweet-button
+  
+  html += '<a target="_blank" href="http://twitter.com/intent/tweet?text='+encodeURIComponent(title)+'+'+encodeURIComponent(text)+' '+encodeURIComponent(url)+'">';
+  html += '<i class="websymbols social_sharing_button" id="twitter_sharing">t</i></a>';
+  return html;
 }
 
 $(function() {
