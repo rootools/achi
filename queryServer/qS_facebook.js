3345f01757866dda8d40737d79e06477952cfef6
rootools <baboonweb@gmail.com>
2012-12-17 20:31:22 +0400

    add fb acivs

--- queryServer/qS_facebook.js
+++ queryServer/qS_facebook.js
@@ -5,7 +5,7 @@ function checkFacebookAchievements(uid, data, db, cb) {
     users: function(callback) {
              db.collection('users_achievements', function(err, collection) {
                collection.findOne({uid:uid, service:'facebook'}, function(err, doc) {
-                 if(doc.achievements == undefined) { doc.achievements = [];}
+                 if(doc.achievements === undefined) { doc.achievements = [];}
                  callback(null, doc.achievements);
                });
              });
@@ -20,7 +20,7 @@ function checkFacebookAchievements(uid, data, db, cb) {
     }, function(err, res) {
       res.all = createAIDarray(res.all);
       res.users = createAIDarray(res.users);
-      if(res.users == undefined || res.users.length == 0) {res.users = [];}
+      if(res.users === undefined || res.users.length === 0) {res.users = [];}
       var notRecieved = res.all;
       for(var i=0;i<notRecieved.length;i++) {
         for(var r=0;r<res.users.length;r++) {
@@ -48,6 +48,55 @@ function fb_4i3ipPZEq88i0bj9ZMiO4I8CGhBHsj(uid, data, aid, db) {
 // Add 50 friends
 function fb_JY88jctEQ9Ze1qk2RG7lPPFXpNdVYT(uid, data, aid, db) {
   if(data.friend_count >= 50) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+// Add 100 friends
+function fb_T1jQDwGManXranbpmOep922uG7sYir(uid, data, aid, db) {
+  if(data.friend_count >= 100) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+// Add Photo
+function fb_0pzom5BuhFOP3G6LwPm0oythCYGv8T(uid, data, aid, db) {
+  if(data.photo_count >= 1) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+// Add 100 Photo
+function fb_gBAyOvu7Yzq9lvQuviF3bWFBRMnGVO(uid, data, aid, db) {
+  if(data.photo_count >= 100) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+// Add 300 Photo
+function fb_mKOk0k81RVEOf5UcH9pFX8OAzwz77S(uid, data, aid, db) {
+  if(data.photo_count >= 300) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+// Add 5 Likes
+function fb_lM92fNOvGl1ZwhSoJenSQAi6ObkRN7(uid, data, aid, db) {
+  if(data.likes_count >= 5) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+// Add 30 Likes
+function fb_RX7VbPD4WxPLsVx5nGicjt70Avvi6r(uid, data, aid, db) {
+  if(data.likes_count >= 30) {
+    writeToDB(uid, aid, db);
+  }
+}
+
+// Add 50 Likes
+function fb_DpJUQvJYEaL0jmqSzGwaQBohSeTgyk(uid, data, aid, db) {
+  if(data.likes_count >= 50) {
     writeToDB(uid, aid, db);
   }
 }
