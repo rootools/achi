f0cdbc66039f7e073d19acc07dd97c809e6e0b60
leninlin <lenin.lin@gmail.com>
2013-03-31 23:06:59 +0400

    add model db

---
+++ models/db.js
@@ -0,0 +1,17 @@
+var config = init.require('config').config.mongo;
+
+var mongodb = require("mongodb");
+var mongoserver = new mongodb.Server(config.host, config.port, config.server_config);
+var db_connector = new mongodb.Db(config.db, mongoserver, config.connector_config);
+
+
+exports.conn = null;
+
+exports.mongoConnect = function () {
+  db_connector.open(function(err, dbs) {
+    conn = dbs;
+  });
+};
+
+
+mongoConnect();
