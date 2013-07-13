c3bf22650647d060d6ae9c28fee1644a2250cc04
Bravin Anton <baboon@ip-10-228-174-15.eu-west-1.compute.internal>
2013-07-13 15:03:21 +0400

    add new steam achivments

--- queryServer/qS_steam.js
+++ queryServer/qS_steam.js
@@ -37,4 +37,44 @@ exports.functions = {
   'Ua0cS1dRO61tsVr9tQlcnmxpIKczLV': function(data) {
     return data.games >= 100;
   },
+  
+  // Spent 3 hours
+  'uFRvENrMALXp0cbp7PFOBjFvzCPKX7': function(data) {
+    return data.maxTime >= 3;
+  },
+  
+  // Spent 10 hours
+  'gog4ZhFuVSOlftHUsbEfv4V0NqXPhO': function(data) {
+    return data.maxTime >= 10;
+  },
+  
+  // Spent 25 hours
+  '18yo4aHrVeJP148DFIVq81k5JQIhpc': function(data) {
+    return data.maxTime >= 25;
+  },
+  
+  // Spent 50 hours
+  'J1Nw8Hb46pZ2LrMWwgNnKeWsB5Ys0T': function(data) {
+    return data.maxTime >= 50;
+  },
+  
+  // Spent 100 hours
+  'RJw4qBN19WmqqC5xzV1PltrW4rXu7q': function(data) {
+    return data.maxTime >= 100;
+  },
+  
+  // Spent 250 hours
+  '7j3CPivAotkWfakZov48EuQYv9UINu': function(data) {
+    return data.maxTime >= 250;
+  },
+  
+  // Spent 500 hours
+  'eigOOMeYOZhhIj8ZwvmQo8fpnUaZfI': function(data) {
+    return data.maxTime >= 500;
+  },
+  
+  // Spent 1000 hours
+  '8PauzohdUvbF6Vn9xxJNtwbqopy9ao': function(data) {
+    return data.maxTime >= 1000;
+  }
 };
