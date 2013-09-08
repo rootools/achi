fdcf132c923199cc9729ea951db1d8f34f3b7067
rootools <baboonweb@gmail.com>
2013-09-08 21:56:29 +0400

    latest

--- daemons/instagram/instagram.js
+++ daemons/instagram/instagram.js
@@ -7,12 +7,16 @@ http.createServer(function (req, res) {
   var query = url.parse(req.url, true).query;
   var response = {};
   request.get('https://api.instagram.com/v1/users/'+query.id+'/?access_token='+query.token, function(e, r, body){
-    body = JSON.parse(body).data.counts;
-    response.media = body.media;
-    response.followed_by = body.followed_by;
-    response.follows = body.follows;
-    console.log(response);
-    res.end(JSON.stringify(response));
+    if(e || JSON.parse(body).meta.error_type) {
+      res.end(JSON.stringify({error: 1}));
+    } else {
+      body = JSON.parse(body).data.counts;
+      response.media = body.media;
+      response.followed_by = body.followed_by;
+      response.follows = body.follows;
+      console.log(response);
+      res.end(JSON.stringify(response));
+    }
   });  
   
 }).listen(process.env.VCAP_APP_PORT || 8035);
